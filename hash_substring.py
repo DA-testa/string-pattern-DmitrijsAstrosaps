55d56ef60aa34cb70de6cea91f0d4f2ac58a252d
Sheepy <125596139+DmitrijsAstrosaps@users.noreply.github.com>
2023-04-15 18:57:26 +0000

    commit hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -28,7 +28,7 @@ def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
+def get_occurrences(Pichu, Turtwig):
     # this function should find the occurances using Rabin Karp alghoritm 
     Raichu = 0
     Torterra = 0
